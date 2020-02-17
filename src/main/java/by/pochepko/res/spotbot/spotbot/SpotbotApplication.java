60acbc16ccab31dde506cbcfced10fb7e364c07d
pochepko_oleg <pochepkooleg@gmail.com>
2020-02-18 01:36:31 +0300

    Initial commit

---
+++ src/main/java/by/pochepko/res/spotbot/spotbot/SpotbotApplication.java
@@ -0,0 +1,13 @@
+package by.pochepko.res.spotbot.spotbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotbotApplication.class, args);
+    }
+
+}
