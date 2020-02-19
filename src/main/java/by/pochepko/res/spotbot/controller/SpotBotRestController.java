d9fdf60879919e2aa11e2111f3391885898ede0d
pochepko_oleg <pochepkooleg@gmail.com>
2020-02-19 13:44:31 +0300

    add tests

--- src/main/java/by/pochepko/res/spotbot/controller/SpotBotRestController.java
+++ src/main/java/by/pochepko/res/spotbot/controller/SpotBotRestController.java
@@ -3,6 +3,7 @@ package by.pochepko.res.spotbot.controller;
 import by.pochepko.res.spotbot.dto.SpotMessageDto;
 import by.pochepko.res.spotbot.service.SpotMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,20 +20,25 @@ public class SpotBotRestController {
         return spotMessageService.getSpotMessage(location);
     }
 
-    @PostMapping(value = "/spotmessages",consumes = "application/json", produces = "application/json")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    @PostMapping(value = "/spotmessages", consumes = "application/json", produces = "application/json")
     @PreAuthorize("hasAuthority('USER')")
     public SpotMessageDto createSpotMessage(@RequestBody SpotMessageDto spotMessageDto){
         return spotMessageService.createSpotMessage(spotMessageDto);
     }
 
     @PutMapping(value = "/spotmessages")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @PreAuthorize("hasAnyAuthority('USER')")
 
-    public void updateSpotMessage(@RequestBody SpotMessageDto spotMessageDto){
+    public void updateSpotMessage(@RequestBody SpotMessageDto spotMessageDto) {
         spotMessageService.updateSpotMessage(spotMessageDto);
     }
 
-    @DeleteMapping void deleteSpotMessage(@RequestBody SpotMessageDto spotMessageDto){
+    @DeleteMapping(value = "/spotmessages")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasAnyAuthority('USER')")
+    void deleteSpotMessage(@RequestBody SpotMessageDto spotMessageDto) {
         spotMessageService.deleteSpotMessage(spotMessageDto);
     }
 }
--- src/test/java/by/pochepko/res/spotbot/controller/SpotBotRestControllerTest.java
+++ src/test/java/by/pochepko/res/spotbot/controller/SpotBotRestControllerTest.java
@@ -4,24 +4,17 @@ import by.pochepko.res.spotbot.SpotbotApplication;
 import by.pochepko.res.spotbot.dto.SpotMessageDto;
 import by.pochepko.res.spotbot.service.SpotMessageService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -42,7 +35,7 @@ class SpotBotRestControllerTest {
         SpotMessageDto spotMessage = new SpotMessageDto("Rome", "Visit Colloseum");
         when(spotMessageService.getSpotMessage("Rome")).thenReturn(spotMessage);
         //then
-        mockMvc.perform(get("/api/spotmessages/Rome"))
+        mockMvc.perform(get("/api/spotmessages/{location}", "Rome"))
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("{\"location\":\"Rome\",\"message\":\"Visit Colloseum\"}")));
 
@@ -52,21 +45,33 @@ class SpotBotRestControllerTest {
     @WithMockUser(username = "user", password = "pswd", authorities = "USER")
     void createSpotMessage_shouldReturnSameDtoAndStatusOk() throws Exception {
         //given
-        SpotMessageDto spotMessage = new SpotMessageDto("Pizzzza", "Visit Colloseum");
+        SpotMessageDto spotMessage = new SpotMessageDto("Rome", "Visit Colloseum");
         when(spotMessageService.createSpotMessage(spotMessage)).thenReturn(spotMessage);
         //when-then
-        mockMvc.perform(post("/api/spotmessages").contentType(MediaType.APPLICATION_JSON).content("{\"location\":\"Pizzzza\",\"message\":\"Visit Colloseum\"}"))
-                .andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("{\"location\":\"Pizzzza\",\"message\":\"Visit Colloseum\"}")));
+        mockMvc.perform(post("/api/spotmessages").contentType(MediaType.APPLICATION_JSON).content("{\"location\":\"Rome\",\"message\":\"Visit Colloseum\"}"))
+                .andDo(print()).andExpect(status().is2xxSuccessful())
+                .andExpect(content().string(containsString("{\"location\":\"Rome\",\"message\":\"Visit Colloseum\"}")));
 
 
     }
 
     @Test
-    void updateSpotMessage() {
+    @WithMockUser(username = "user", password = "pswd", authorities = "USER")
+    void updateSpotMessage_shouldReturnStatusOk() throws Exception {
+        //given
+        SpotMessageDto spotMessage = new SpotMessageDto("Rome", "Visit Colloseum");
+        //when-then
+        mockMvc.perform(put("/api/spotmessages").contentType(MediaType.APPLICATION_JSON).content("{\"location\":\"Rome\",\"message\":\"Visit Colloseum\"}"))
+                .andDo(print()).andExpect(status().is2xxSuccessful());
+
+
     }
 
     @Test
-    void deleteSpotMessage() {
+    @WithMockUser(username = "user", password = "pswd", authorities = "USER")
+    void deleteSpotMessage_shouldReturnStatusOk() throws Exception {
+        mockMvc.perform(delete("/api/spotmessages").contentType(MediaType.APPLICATION_JSON).content("{\"location\":\"Rome\",\"message\":\"Visit Colloseum\"}"))
+                .andDo(print()).andExpect(status().is2xxSuccessful());
+
     }
 }
